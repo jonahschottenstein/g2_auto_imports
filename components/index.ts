82cf35e8c1e3dbd3d7c3eca3d673819ba7bb2642
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-05-29 15:51:48 -0600

    Import & export CarCard

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,6 @@
 import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import NavBar from "./NavBar";
+import CarCard from "./CarCard";
 
-export { Hero, CustomButton, NavBar };
+export { Hero, CustomButton, NavBar, CarCard };
