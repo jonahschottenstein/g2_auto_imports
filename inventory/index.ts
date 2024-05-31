5d58796e46be716f2d9ea4e1891c1335afe3469d
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-05-31 13:29:20 -0600

    Add inventory array of Cars

---
+++ inventory/index.ts
@@ -0,0 +1,24 @@
+import { Car } from "@/types";
+
+export const inventory: Car[] = [
+	{
+		id: "1",
+		imageSrc: "/tokyo-imports-hero-image.png",
+		year: 1993,
+		make: "Mitsubishi",
+		model: "Delica L300",
+		price: "$5,000",
+		pageUrl: "",
+		featured: true,
+	},
+	{
+		id: "2",
+		imageSrc: "/tokyo-imports-hero-image.png",
+		year: 1994,
+		make: "Mitsubishi",
+		model: "Delica L400",
+		price: "$6,000",
+		pageUrl: "",
+		featured: true,
+	},
+];
