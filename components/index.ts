6c17115b69187209c1ed6a6c48509f88f4270ff6
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-05-31 11:07:22 -0600

    Import and export Footer.tsx

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,7 @@
 import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import NavBar from "./NavBar";
+import Footer from "./Footer";
 import CarCard from "./CarCard";
 
-export { Hero, CustomButton, NavBar, CarCard };
+export { Hero, CustomButton, NavBar, Footer, CarCard };
