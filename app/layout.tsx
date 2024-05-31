cfeba7ddbc5f11f0231589fec8f354b0a16f83b0
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-05-31 11:15:37 -0600

    Add Footer below children in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import PrelineScript from "@/components/PrelineScript";
-import { NavBar } from "@/components";
+import { Footer, NavBar } from "@/components";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +21,7 @@ export default function RootLayout({
 			<body className={inter.className}>
 				<NavBar />
 				{children}
+				<Footer />
 			</body>
 			<PrelineScript />
 		</html>
