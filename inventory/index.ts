857d19f4f583f319223488f50b660fc95af80948
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-06-01 17:31:56 -0600

    Update inventory properties

--- inventory/index.ts
+++ inventory/index.ts
@@ -2,23 +2,23 @@ import { Car } from "@/types";
 
 export const inventory: Car[] = [
 	{
-		id: "1",
-		imageSrc: "/tokyo-imports-hero-image.png",
+		id: "P35W-0901053",
+		imageSrc: "/P35-0901053_07.jpg",
 		year: 1993,
 		make: "Mitsubishi",
 		model: "Delica L300",
 		price: "$5,000",
-		pageUrl: "",
+		pageUrl: "/vehicle/P35W-0901053",
 		featured: true,
 	},
 	{
-		id: "2",
-		imageSrc: "/tokyo-imports-hero-image.png",
+		id: "PEDW-0402421",
+		imageSrc: "/PE8W-0402421_04.JPG",
 		year: 1994,
 		make: "Mitsubishi",
 		model: "Delica L400",
 		price: "$6,000",
-		pageUrl: "",
+		pageUrl: "/vehicle/PEDW-0402421",
 		featured: true,
 	},
 ];
