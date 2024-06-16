4b57d12aa39c3c81fd40486c9556dcb100fcd6d2
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-06-15 18:38:55 -0600

    Set padding-top to NAV_BAR_HEIGHT

--- app/request-import-form/layout.tsx
+++ app/request-import-form/layout.tsx
@@ -1,13 +1,15 @@
 import { FormProvider } from "@/context/request-import-form-context";
 import React from "react";
 
+const NAV_BAR_HEIGHT = `72px`;
+
 const Layout = ({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) => {
 	return (
-		<main>
+		<main className={`pt-[${NAV_BAR_HEIGHT}]`}>
 			<FormProvider>{children}</FormProvider>
 		</main>
 	);
