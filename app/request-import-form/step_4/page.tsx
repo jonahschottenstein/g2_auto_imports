99cc1d4049a13e0a9027860b6ed7c0b238ae1647
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-06-21 12:06:43 -0600

    Add ContactForm to step_4 page

--- app/request-import-form/step_4/page.tsx
+++ app/request-import-form/step_4/page.tsx
@@ -1,7 +1,12 @@
+import ContactForm from "@/components/ContactForm";
 import React from "react";
 
 const page = () => {
-	return <div>Step 4</div>;
+	return (
+		<div className="form-page flex flex-col flex-1 overflow-auto">
+			<ContactForm />
+		</div>
+	);
 };
 
 export default page;
