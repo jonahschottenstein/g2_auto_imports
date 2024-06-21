09858621e8c82a52b82385a0f0e0fcf4370a2afa
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-06-21 12:10:52 -0600

    Add FormField component

---
+++ components/FormField.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { FormFieldProps } from "@/types";
+
+const FormField = ({
+	label,
+	type,
+	inputId,
+	name,
+	register,
+	error,
+	valueAsNumber,
+	rule,
+}: FormFieldProps) => {
+	return (
+		<div className="form-field-container flex flex-col">
+			<label htmlFor={inputId} className="form-field-label">
+				{label}
+			</label>
+			<input
+				type={type}
+				id={inputId}
+				className="p-1 bg-gray-400"
+				{...register(name, { valueAsNumber })}
+			/>
+			{rule && <p className="rule text-xs">{rule}</p>}
+			{error && <span className="error-message">{error.message}</span>}
+		</div>
+	);
+};
+
+export default FormField;
