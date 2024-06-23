a84f8ef808f97eab0be5c2aa2528ac094841d6bd
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-06-23 12:59:26 -0600

    Add Link components to Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import { CustomButton } from ".";
 import Link from "next/link";
+import { PrimaryBlockLink, SecondaryBlockLink } from "./CustomLinks";
 
 const Hero = () => {
 	const NAV_BAR_HEIGHT = "72px";
@@ -9,9 +10,6 @@ const Hero = () => {
 
 	return (
 		<div
-			// style={{ minHeight: `calc(100vh - ${NAV_BAR_HEIGHT})` }}
-			// className="hero flex flex-1 relative w-full h-screen min-h-screen sm:items-end"
-			// className={`hero flex flex-1 relative w-full h-screen min-h-[calc(100vh-72px)] items-end`}
 			className={`hero flex flex-1 relative w-full h-screen min-h-[calc(100vh-${NAV_BAR_HEIGHT})] items-end sm:min-h-screen`}>
 			<Image
 				src={"/tokyo-imports-hero-image.png"}
@@ -24,16 +22,11 @@ const Hero = () => {
 				}}
 			/>
 			<div className="hero-cta-container flex flex-col sm:flex-row sm:h-fit sm:justify-start gap-4 justify-end m-8 w-full z-10">
-				{/* <CustomButton
+				<PrimaryBlockLink
+					href="/request-import-form/step_1"
 					title="Request Import"
-					styles="bg-black text-white px-4 py-2 rounded-lg text-base"
-				/> */}
-				{/* <Link href={"/request-import"}>Request Import</Link> */}
-				<Link href={"/request-import-form/step_1"}>Request Import</Link>
-				<CustomButton
-					title="View Inventory"
-					styles="bg-white text-black px-4 py-2 rounded-lg text-base"
 				/>
+				<SecondaryBlockLink href="/" title="View Inventory" />
 			</div>
 		</div>
 	);
