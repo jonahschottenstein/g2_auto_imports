d3866a6f2039c21757665f752a27349f71477013
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-06-23 22:23:00 -0600

    Add page component

---
+++ app/request-import-form/step_5/page.tsx
@@ -0,0 +1,12 @@
+import FormReview from "@/components/FormReview";
+import React from "react";
+
+const page = () => {
+	return (
+		<div className="form-page flex flex-col flex-1 overflow-auto">
+			<FormReview />
+		</div>
+	);
+};
+
+export default page;
