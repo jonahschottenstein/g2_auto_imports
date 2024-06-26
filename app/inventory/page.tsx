2d7f76c031c3b3fa48aeaa480a73269c4a7d859a
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-06-26 15:18:54 -0600

    Add page component

---
+++ app/inventory/page.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { inventory } from "@/inventory";
+import { CarCard } from "@/components";
+
+const page = () => {
+	return (
+		<main className="pt-[72px] flex-1">
+			<h1 className="text-center text-2xl mt-8">Inventory</h1>
+			<ul className="inventory-container p-8 grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+				{inventory.map(
+					({ imageSrc, year, make, model, price, pageUrl }, index) => (
+						<li key={`${year}-${make}-${model}-${index}`} className="">
+							<CarCard
+								imageSrc={imageSrc}
+								year={year}
+								make={make}
+								model={model}
+								price={price}
+								pageUrl={pageUrl}
+							/>
+						</li>
+					)
+				)}
+			</ul>
+		</main>
+	);
+};
+
+export default page;
