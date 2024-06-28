a3c44cee174bee0a6898c58ac030d61d48494472
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-06-28 13:22:45 -0600

    Add PostRequest component content

--- components/PostRequest.tsx
+++ components/PostRequest.tsx
@@ -1,7 +1,24 @@
+"use client";
+
+import { useForm } from "@/context/request-import-form-context";
 import React from "react";
 
 const PostRequest = () => {
-	return <div>PostRequest</div>;
+	const user = useForm();
+
+	return (
+		<div className="form-container px-8 h-full flex flex-col">
+			<h1 className="text-center text-2xl my-4">
+				Your import request has been submitted!
+			</h1>
+			<div>
+				<p>
+					You will get an email confirmation at{" "}
+					<span className="font-bold">{user.contactInfo?.email}</span>
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default PostRequest;
