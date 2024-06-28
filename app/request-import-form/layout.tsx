a3efd1d52108707c44c7d6690ba99706e5886752
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-06-28 10:23:04 -0600

    Remove FormStepper and set min-height to 100%

--- app/request-import-form/layout.tsx
+++ app/request-import-form/layout.tsx
@@ -13,9 +13,9 @@ const Layout = ({
 	return (
 		// <main className={`pt-[${NAV_BAR_HEIGHT}]`}>
 		// <main className={`pt-[72px] h-[calc(100%-24px)] flex flex-col`}>
-		<main className={`pt-[72px] h-full flex flex-col`}>
-			<h1 className="text-center text-3xl text-bold mt-4">Request Import</h1>
-			<FormStepper steps={["Make", "Model", "Year(s)", "Contact", "Review"]} />
+		<main className={`pt-[72px] h-full min-h-full flex flex-col`}>
+			{/* <h1 className="text-center text-3xl text-bold mt-4">Request Import</h1> */}
+			{/* <FormStepper steps={["Make", "Model", "Year(s)", "Contact", "Review"]} /> */}
 			<FormProvider>{children}</FormProvider>
 		</main>
 	);
