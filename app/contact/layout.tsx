8157ac929d05f2165fff920687e52f7668b77c2a
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-07-05 10:31:58 -0600

    Add Layout component for contact pages

---
+++ app/contact/layout.tsx
@@ -0,0 +1,19 @@
+import { FormProvider } from "@/context/request-import-form-context";
+import React from "react";
+
+const NAV_BAR_HEIGHT = `72px`;
+const FOOTER_HEIGHT = `24px`;
+
+const Layout = ({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) => {
+	return (
+		<main className={`pt-[72px] h-full min-h-full flex flex-col`}>
+			<FormProvider>{children}</FormProvider>
+		</main>
+	);
+};
+
+export default Layout;
