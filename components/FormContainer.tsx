27ef895ca1c6518bd308b05b5fd5d2c8707d0aa8
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-07-05 10:26:45 -0600

    Add max-width to FormContainer

--- components/FormContainer.tsx
+++ components/FormContainer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const FormContainer = ({ h1, children }: FormContainerProps) => {
 	return (
-		<div className="form-container px-8 h-[calc(100%-92px)] flex flex-col min-w-96 w-full">
+		<div className="form-container px-8 h-[calc(100%-92px)] flex flex-col min-w-96 max-w-lg w-full">
 			<h1 className="text-center text-2xl mb-4">{h1}</h1>
 			{children}
 		</div>
