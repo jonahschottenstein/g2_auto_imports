01e2856d610fc0b5b608e0894f990004062def0e
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-07-12 16:54:58 -0600

    Change styles to fix page scroll

--- app/request-import-form/step_5/page.tsx
+++ app/request-import-form/step_5/page.tsx
@@ -6,7 +6,8 @@ const page = () => {
 	return (
 		<div
 			// className="form-page flex flex-col flex-1 items-center overflow-x-hidden overflow-y-auto"
-			className="form-page flex flex-col flex-1 items-center overflow-x-hidden overflow-y-clip">
+			// className="form-page flex flex-col flex-1 items-center overflow-x-hidden overflow-y-clip"
+			className="form-page flex flex-col flex-1 items-center overflow-x-hidden">
 			<FormStepper steps={["Make", "Model", "Year(s)", "Contact", "Review"]} />
 			<FormReview />
 		</div>
