dd1acd75ec463203c56c3b864ecc4cc474325f33
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-07-16 15:59:47 -0600

    Add image zoom effect on hover

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -13,15 +13,17 @@ const CarCard = ({
 }: CarCardProps) => {
 	return (
 		<Link
-			className="flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-lg transition dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70"
+			className="flex flex-col group bg-white border shadow-sm rounded-xl overflow-hidden hover:shadow-lg transition dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70"
 			href={pageUrl}>
-			<Image
-				className="w-full h-auto rounded-t-xl"
-				src={imageSrc}
-				alt={`Image of ${year} ${make} ${model}`}
-				width={100}
-				height={100}
-			/>
+			<div className="relative pt-[50%] sm:pt-[60%] lg:pt-[80%] rounded-t-xl overflow-hidden">
+				<Image
+					className="size-full absolute top-0 start-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-t-xl"
+					src={imageSrc}
+					alt={`Image of ${year} ${make} ${model}`}
+					width={100}
+					height={100}
+				/>
+			</div>
 			<div className="p-4 md:p-5">
 				<h3 className="text-lg font-bold text-gray-800 dark:text-white">
 					{`${year} ${make} ${model}`}
