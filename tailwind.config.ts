fc0b6e46d4fb76db15c199a3a467443f96db0299
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-07-17 11:39:42 -0600

    Extend fontFamily property to add display and sans classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,10 @@ const config: Config = {
 				"gradient-conic":
 					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
 			},
+			fontFamily: {
+				display: ["var(--font-montserrat)"],
+				sans: ["var(--font-roboto-flex)"],
+			},
 		},
 	},
 	plugins: [require("preline/plugin")],
