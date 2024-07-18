03bfe6eabee10e146e953e909689592777b26117
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-07-18 16:23:05 -0600

    Update CarCard styles to maintain aspect ratio

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -3,7 +3,7 @@ import { CarCardProps } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 
-const CarCard = ({
+/* const CarCard = ({
 	imageSrc,
 	year,
 	make,
@@ -13,7 +13,7 @@ const CarCard = ({
 }: CarCardProps) => {
 	return (
 		<Link
-			className="flex flex-col group bg-white border shadow-sm rounded-xl overflow-hidden hover:shadow-lg transition dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70"
+			className="flex flex-col group bg-white border shadow-sm rounded-xl overflow-hidden hover:shadow-lg transition "
 			href={pageUrl}>
 			<div className="relative pt-[50%] sm:pt-[60%] lg:pt-[80%] rounded-t-xl overflow-hidden">
 				<Image
@@ -25,10 +25,39 @@ const CarCard = ({
 				/>
 			</div>
 			<div className="p-4 md:p-5">
-				<h3 className="text-lg font-bold text-gray-800 dark:text-white">
+				<h3 className="text-lg font-bold text-gray-800 ">
 					{`${year} ${make} ${model}`}
 				</h3>
-				<p className="mt-1 text-gray-500 dark:text-neutral-400">{price}</p>
+				<p className="mt-1 text-gray-500 ">{price}</p>
+			</div>
+		</Link>
+	);
+}; */
+
+const CarCard = ({
+	imageSrc,
+	year,
+	make,
+	model,
+	price,
+	pageUrl,
+}: CarCardProps) => {
+	return (
+		<Link
+			className="car-card flex flex-col shadow-md rounded-lg overflow-hidden transform transition-all duration-300 hover:scale-105 hover:shadow-lg"
+			href={pageUrl}>
+			<div className="relative pb-[66.6667%]">
+				<Image
+					className="absolute top-0 left-0 w-full h-full object-cover transition-transform duration-300"
+					src={imageSrc}
+					alt={`Image of ${year} ${make} ${model}`}
+					width={100}
+					height={100}
+				/>
+			</div>
+			<div className="p-4">
+				<h3 className="car-name text-xl text-gray-800 font-bold font-display uppercase">{`${year} ${make} ${model}`}</h3>
+				<p className="text-lg font-sans text-gray-600">{`${price}`}</p>
 			</div>
 		</Link>
 	);
