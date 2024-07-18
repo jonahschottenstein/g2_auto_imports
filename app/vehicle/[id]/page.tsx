b1e14b68f1308c4e6c9621b98f2a953645d40042
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-07-18 16:39:52 -0600

    Update page, Image, and table styles

--- app/vehicle/[id]/page.tsx
+++ app/vehicle/[id]/page.tsx
@@ -18,10 +18,12 @@ const Page = ({ params }: { params: { id: string } }) => {
 	return (
 		<main className="flex-1 pt-[72px]">
 			<div className="p-8 flex flex-col">
-				<h1 className="text-center text-2xl">{carTitle}</h1>
-				<div className="car-page flex flex-col gap-8 my-8 sm:flex-row">
+				<h1 className="text-center text-5xl mb-4 font-display font-bold uppercase">
+					{carTitle}
+				</h1>
+				<div className="car-page flex flex-col gap-8 my-8 md:flex-row">
 					<Image
-						className="w-full h-auto sm:w-8/12"
+						className="w-full h-auto md:w-8/12 object-contain self-start"
 						src={vehicleDetails?.imageSrc || "/"}
 						alt={`Image of ${carTitle}`}
 						width={100}
@@ -29,19 +31,31 @@ const Page = ({ params }: { params: { id: string } }) => {
 						priority
 					/>
 					{vehicleDetails ? (
-						<table className="h-fit flex-1">
-							<thead>
-								<tr>
+						<table className="flex-1">
+							{/* <thead>
+								<tr className="font-display">
 									<th colSpan={2}>Vehicle Details</th>
 								</tr>
-							</thead>
+							</thead> */}
 							<tbody>
 								{Object.entries(vehicleDetails).map(([key, value]) => {
 									if (displayKeys.includes(key)) {
 										return (
-											<tr className="border-b-2 border-gray-400">
-												<td>{key.slice(0, 1).toUpperCase() + key.slice(1)}</td>
-												<td>{value}</td>
+											<tr className="first:border-t border-b border-gray-300">
+												<td className="py-2 text-gray-700 font-display text-sm uppercase">
+													{key}
+												</td>
+												<td className="py-2 text-gray-600 font-sans font-bold text-base">
+													{key === "features" ? (
+														<ul className="list-disc pl-4">
+															{value.map((feature: string) => (
+																<li>{feature}</li>
+															))}
+														</ul>
+													) : (
+														value
+													)}
+												</td>
 											</tr>
 										);
 									}
