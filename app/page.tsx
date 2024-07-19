8ff19b8e8b4d8c068f9f7f567af82c3c09e5c4e9
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-07-19 14:57:28 -0600

    Refactor home page using preline components

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,103 @@
 import { CarCard, Hero } from "@/components";
 import { inventory } from "@/inventory";
+import Link from "next/link";
+import Image from "next/image";
+import { Car } from "@/types";
+
+interface CardProps {
+	href: string;
+	src: string;
+	alt: string;
+	title: string;
+	description: string;
+}
+
+const Card = ({ href, src, alt, title, description }: CardProps) => {
+	return (
+		<Link className="group block" href={href}>
+			<div className="aspect-w-16 aspect-h-12 overflow-hidden bg-gray-100 rounded-2xl dark:bg-neutral-800">
+				<Image
+					className="group-hover:scale-105 transition-transform duration-500 ease-in-out object-cover rounded-2xl"
+					src={src}
+					alt={alt}
+					width={200}
+					height={200}
+				/>
+			</div>
+
+			<div className="pt-4">
+				<h3 className="relative inline-block font-medium font-sans text-lg text-black before:absolute before:bottom-0.5 before:start-0 before:-z-[1] before:w-full before:h-1 before:bg-lime-400 before:transition before:origin-left before:scale-x-0 group-hover:before:scale-x-100 dark:text-white">
+					{title}
+				</h3>
+				<p className="mt-1 font-sans text-gray-600 dark:text-neutral-400">
+					{description}
+				</p>
+			</div>
+		</Link>
+	);
+};
+
+interface CardGridProps {
+	children: React.ReactNode;
+}
+
+const CardGrid = ({ children }: CardGridProps) => {
+	return (
+		<div className=" grid grid-cols-2 gap-4 sm:gap-6 md:gap-8 lg:gap-12">
+			{children}
+		</div>
+	);
+};
+
+interface FeaturedInventorySectionProps {
+	featuredInventory: Car[];
+}
+
+const FeaturedInventorySection = ({
+	featuredInventory,
+}: FeaturedInventorySectionProps) => {
+	return (
+		// TODO: Should probably change div to section
+		<div className="featured-inventory-section w-full max-w-7xl px-4 lg:px-6 lg:px-8 py-12 lg:py-24 mx-auto">
+			<div className="mb-6 sm:mb-10 max-w-2xl text-center mx-auto">
+				<h2 className="font-medium font-display text-black text-2xl uppercase sm:text-4xl dark:text-white">
+					Featured Inventory
+				</h2>
+			</div>
+			<CardGrid>
+				{featuredInventory.map(
+					({ imageSrc, year, make, model, price, pageUrl }) => {
+						return (
+							<Card
+								key={`${year}-${make}-${model}-card`}
+								href={pageUrl}
+								src={imageSrc}
+								alt="Alt text"
+								title={`${year} ${make} ${model}`}
+								description={price}
+							/>
+						);
+					}
+				)}
+			</CardGrid>
+			<div className="mt-10 lg:mt-20 text-center">
+				<a
+					className="relative inline-block font-medium md:text-lg font-sans text-black before:absolute before:bottom-0.5 before:start-0 before:-z-[1] before:w-full before:h-1 before:bg-lime-400 hover:before:bg-black focus:outline-none focus:before:bg-black dark:text-white dark:hover:before:bg-white dark:focus:before:bg-white"
+					href="/inventory">
+					View all inventory
+				</a>
+			</div>
+		</div>
+	);
+};
 
 export default async function Home() {
 	const featuredInventory = inventory.filter((car) => car.featured);
 
 	return (
-		<main className="flex flex-col flex-1">
+		<main className="flex flex-col flex-1 pt-[32px]">
 			<Hero />
-			<section className="featured-inventory-section w-full p-8">
-				<h2 className="text-center text-3xl text-gray-900 font-bold font-display uppercase mb-6">
-					Featured Inventory
-				</h2>
-				<ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-					{featuredInventory.map(
-						({ imageSrc, year, make, model, price, pageUrl }, index) => (
-							<li key={`${year}-${make}-${model}-${index}`}>
-								<CarCard
-									imageSrc={imageSrc}
-									year={year}
-									make={make}
-									model={model}
-									price={price}
-									pageUrl={pageUrl}
-								/>
-							</li>
-						)
-					)}
-				</ul>
-			</section>
+			<FeaturedInventorySection featuredInventory={featuredInventory} />
 		</main>
 	);
 }
