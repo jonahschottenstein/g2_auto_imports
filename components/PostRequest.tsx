f2f6ede22ef299fe308bc047e9b680ab3a8b00e1
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-07-23 15:22:51 -0600

    Use Return Home button to clear sessionStorage

--- components/PostRequest.tsx
+++ components/PostRequest.tsx
@@ -2,20 +2,19 @@
 
 import { useForm, useFormUpdater } from "@/context/request-import-form-context";
 import React, { useEffect } from "react";
+import CustomButton from "./CustomButton";
+import { useRouter } from "next/navigation";
 
 const PostRequest = () => {
 	const user = useForm();
 	const updateUserData = useFormUpdater();
+	const router = useRouter();
 
 	useEffect(() => {
 		const storedUserData = sessionStorage.getItem("userData");
 		const userData = storedUserData && JSON.parse(storedUserData);
 
 		updateUserData(userData);
-	}, []);
-
-	useEffect(() => {
-		sessionStorage.clear();
 	}, []);
 
 	return (
@@ -29,6 +28,14 @@ const PostRequest = () => {
 					<span className="font-bold">{user.contactInfo?.email}</span>
 				</p>
 			</div>
+			<CustomButton
+				children="Return Home"
+				styles="mt-[29rem] mx-auto w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none sm:w-60 font-sans"
+				handleClick={() => {
+					router.push("/");
+					sessionStorage.clear();
+				}}
+			/>
 		</div>
 	);
 };
