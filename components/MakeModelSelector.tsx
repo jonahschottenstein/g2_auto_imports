ff46411091909cb0d3f3df476e8530375cbf30c9
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-07-24 10:38:52 -0600

    Add method to auto-scroll to selected option

--- components/MakeModelSelector.tsx
+++ components/MakeModelSelector.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useLayoutEffect, useRef } from "react";
 import { MakeModelSelectorProps } from "@/types";
 
 const sortMakeModel = (options: MakeModelSelectorProps["options"]) => {
@@ -13,7 +13,21 @@ const MakeModelSelector = ({
 	stateValue,
 	handleChange,
 }: MakeModelSelectorProps) => {
+	const selectedMakeRef = useRef<null | HTMLDivElement>(null);
 	const sortedOptions = sortMakeModel(options);
+
+	const handleScrollToSelectedMake = () => {
+		if (!selectedMakeRef.current) return;
+
+		selectedMakeRef.current.scrollIntoView({
+			behavior: "instant",
+			block: "end",
+		});
+	};
+
+	useLayoutEffect(() => {
+		handleScrollToSelectedMake();
+	}, [stateValue]);
 
 	// * 371px is the max-height that makes the page height full
 
@@ -27,29 +41,35 @@ const MakeModelSelector = ({
 			[&::-webkit-scrollbar-thumb]:bg-gray-300
 			dark:[&::-webkit-scrollbar-track]:bg-neutral-700
 			dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
-			{sortedOptions.map((option) => (
-				<div
-					key={option.name}
-					/* className="option-container border-y-2 hover:bg-slate-500 active:bg-slate-600" */
-					className="option-container border-y-2">
-					<label
-						// htmlFor={`${category}-${option.id}`}
-						htmlFor={`${option.id}`}
-						className="block relative w-full h-full p-2 has-[:checked]:bg-blue-400">
-						<input
-							type="radio"
-							// id={`${category}-${option.id}`}
-							id={`${option.id}`}
-							name={category}
-							value={option.name}
-							checked={stateValue === option.name}
-							onChange={handleChange}
-							className={`${category}-option absolute h-0 w-0 top-0 left-0 opacity-0`}
-						/>
-						{option.name}
-					</label>
-				</div>
-			))}
+			{sortedOptions.map((option) => {
+				const itemProps =
+					stateValue === option.name ? { ref: selectedMakeRef } : {};
+
+				return (
+					<div
+						key={option.name}
+						/* className="option-container border-y-2 hover:bg-slate-500 active:bg-slate-600" */
+						className="option-container border-y-2"
+						{...itemProps}>
+						<label
+							// htmlFor={`${category}-${option.id}`}
+							htmlFor={`${option.id}`}
+							className="block relative w-full h-full p-2 has-[:checked]:bg-blue-400">
+							<input
+								type="radio"
+								// id={`${category}-${option.id}`}
+								id={`${option.id}`}
+								name={category}
+								value={option.name}
+								checked={stateValue === option.name}
+								onChange={handleChange}
+								className={`${category}-option absolute h-0 w-0 top-0 left-0 opacity-0`}
+							/>
+							{option.name}
+						</label>
+					</div>
+				);
+			})}
 		</div>
 	);
 };
