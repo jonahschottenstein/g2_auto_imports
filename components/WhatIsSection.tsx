c0198a979bdb3de1d9bfb24526ab8f80cf508608
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-08-11 17:01:14 +0300

    Add WhatIsSection for home page

---
+++ components/WhatIsSection.tsx
@@ -0,0 +1,106 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faBox,
+	faBriefcase,
+	faCar,
+	faChartSimple,
+} from "@fortawesome/free-solid-svg-icons";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+
+interface IconBlockProps {
+	icon: IconProp;
+	title: string;
+	blurb: string;
+}
+
+interface IconBlockArrayProps {
+	iconBlocks: IconBlockProps[];
+}
+
+const iconBlocks: IconBlockProps[] = [
+	{
+		icon: faCar as IconProp,
+		title: "Authentic JDM Cars",
+		blurb:
+			"Tokyo-Imports specializes in importing genuine Japanese Domestic Market vehicles directly from Japan.",
+	},
+	{
+		icon: faBox as IconProp,
+		title: "Hassle-Free Importing",
+		blurb:
+			"We handle all the complex paperwork and logistics to ensure a smooth and stress-free car import experience.",
+	},
+	{
+		icon: faChartSimple as IconProp,
+		title: "Detailed Listings",
+		blurb:
+			"Each car listing includes comprehensive details and high-quality images to help you make informed decisions.",
+	},
+	{
+		icon: faBriefcase as IconProp,
+		title: "Professional Expertise",
+		blurb:
+			"Our team has years of experience in the car import industry, providing expert advice and support throughout the process.",
+	},
+];
+
+const IconBlockComponent = ({ icon, title, blurb }: IconBlockProps) => {
+	return (
+		<div className="flex gap-x-5">
+			{/* <FontAwesomeIcon className="shrink-0 fw w-7" icon={icon} size="sm" /> */}
+			<FontAwesomeIcon
+				className="shrink-0 mt-1 size-6 fw text-blue-600"
+				icon={icon}
+				size="sm"
+			/>
+			<div className="grow">
+				<h3 className="text-lg font-semibold font-sans text-gray-800">
+					{title}
+				</h3>
+				<p className="mt-1 font-sans text-gray-600">{blurb}</p>
+			</div>
+		</div>
+	);
+};
+
+const IconBlockGrid = ({ iconBlocks }: IconBlockArrayProps) => {
+	return (
+		<div className="mt-5 lg:mt-16 grid lg:grid-cols-3 gap-8 lg:gap-12">
+			<div className="lg:col-span-1">
+				<h2 className="font-sans font-bold text-2xl md:text-3xl text-gray-800">
+					Your Gateway to Authentic JDM Imports
+				</h2>
+				<p className="mt-2 md:mt-4 font-sans text-gray-500">
+					Tokyo-Imports is your trusted partner for bringing the best Japanese
+					Domestic Market vehicles directly to your driveway, offering a
+					seamless and informed import experience from start to finish.
+				</p>
+			</div>
+			<div className="lg:col-span-2">
+				<div className="grid sm:grid-cols-2 gap-8 md:gap-12">
+					{iconBlocks.map(({ icon, title, blurb }) => {
+						return (
+							<IconBlockComponent icon={icon} title={title} blurb={blurb} />
+						);
+					})}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+const WhatIsSection = () => {
+	return (
+		<div className="what-is-section w-full max-w-7xl px-4 md:px-6 lg:px-8 py-12 lg:py-24 mx-auto">
+			<div className="mb-6 sm:mb-10 max-w-2xl text-center mx-auto">
+				<h2 className="font-medium font-display text-black text-2xl uppercase sm:text-4xl ">
+					What's Tokyo-Imports?
+				</h2>
+			</div>
+			<IconBlockGrid iconBlocks={iconBlocks} />
+		</div>
+	);
+};
+
+export default WhatIsSection;
