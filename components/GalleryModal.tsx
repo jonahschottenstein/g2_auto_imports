94fd09bb7c2db8f66edc000dfb9f9b382ccbf681
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-08-17 18:29:15 -0600

    Update GalleryModal button styles

--- components/GalleryModal.tsx
+++ components/GalleryModal.tsx
@@ -74,7 +74,7 @@ const GalleryModal = () => {
 		<div className="gallery-modal">
 			<button
 				type="button"
-				className="gallery-modal-button py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600/50 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none absolute top-0 left-0 bottom-0 right-0"
+				className="gallery-modal-button py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600/50 text-white hover:bg-blue-700/60 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none absolute top-0 left-0 bottom-0 right-0 font-sans justify-center"
 				aria-haspopup="dialog"
 				aria-expanded="false"
 				aria-controls="hs-vertically-centered-scrollable-modal"
