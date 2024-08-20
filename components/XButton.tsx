f9bb9a36a8128286cb0fad854e26fc403f536e9f
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-08-20 14:44:57 -0600

    Initial commit

---
+++ components/XButton.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+
+interface XButtonProps {
+	onClose: () => void;
+	styles: string;
+}
+
+const XButton = ({ onClose, styles }: XButtonProps) => {
+	return (
+		<button onClick={onClose} className={styles}>
+			<FontAwesomeIcon icon={faXmark as IconProp} className="size-6" />
+		</button>
+	);
+};
+
+export default XButton;
