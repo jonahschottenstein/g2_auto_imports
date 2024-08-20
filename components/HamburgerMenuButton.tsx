7f29f8355e003bec0bc331fe43a22d3f62c00c97
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-08-20 17:31:25 -0600

    Initial commit

---
+++ components/HamburgerMenuButton.tsx
@@ -0,0 +1,15 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+
+interface HamburgerMenuButtonProps {
+	onClick: () => void;
+}
+
+export const HamburgerMenuButton = ({ onClick }: HamburgerMenuButtonProps) => {
+	return (
+		<button onClick={onClick} className="flex focus:outline-none">
+			<FontAwesomeIcon icon={faBars as IconProp} className="size-6" />
+		</button>
+	);
+};
