ecd4130c3a82e70d0143a144f21d428e18de4658
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-08-23 16:30:42 -0600

    Create new layout for Hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { CustomButton } from ".";
 import Link from "next/link";
 import { PrimaryBlockLink, SecondaryBlockLink } from "./CustomLinks";
+import heroImage from "../public/tokyo-imports-hero-image.png";
 
 const HeroOld = () => {
 	const NAV_BAR_HEIGHT = "72px";
@@ -33,9 +34,8 @@ const HeroOld = () => {
 };
 
 // TODO: Need to break this up into components
-const Hero = () => {
+/* const Hero = () => {
 	return (
-		// <!-- Slider -->
 		<div className="px-4 md:px-6 lg:px-8 py-10 pt-14">
 			<div
 				data-hs-carousel='{
@@ -44,7 +44,6 @@ const Hero = () => {
 				className="relative">
 				<div className="hs-carousel relative overflow-hidden w-full h-[30rem] md:h-[calc(100vh-106px)]  bg-gray-100 rounded-2xl">
 					<div className="hs-carousel-body absolute top-0 bottom-0 start-0 flex flex-nowrap transition-transform duration-700 opacity-0 font-sans">
-						{/* <!-- Item --> */}
 						<div className="hs-carousel-slide">
 							<div className="h-[30rem] md:h-[calc(100vh-106px)]  flex flex-col bg-[url('/tokyo-imports-hero-image.png')] bg-cover bg-center bg-no-repeat">
 								<div className="mt-auto w-2/3 md:max-w-lg ps-5 pb-5 md:ps-10 md:pb-10">
@@ -62,9 +61,7 @@ const Hero = () => {
 								</div>
 							</div>
 						</div>
-						{/* <!-- End Item --> */}
 
-						{/* <!-- Item --> */}
 						<div className="hs-carousel-slide">
 							<div className="h-[30rem] md:h-[calc(100vh-106px)]  flex flex-col bg-[url('https://images.unsplash.com/photo-1612287230202-1ff1d85d1bdf?q=80&w=3542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-center bg-no-repeat">
 								<div className="mt-auto w-2/3 md:max-w-lg ps-5 pb-5 md:ps-10 md:pb-10">
@@ -82,9 +79,7 @@ const Hero = () => {
 								</div>
 							</div>
 						</div>
-						{/* <!-- End Item --> */}
 
-						{/* <!-- Item --> */}
 						<div className="hs-carousel-slide">
 							<div className="h-[30rem] md:h-[calc(100vh-106px)]  flex flex-col bg-[url('https://images.unsplash.com/photo-1629666451094-8908989cae90?q=80&w=3464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-center bg-no-repeat">
 								<div className="mt-auto w-2/3 md:max-w-lg ps-5 pb-5 md:ps-10 md:pb-10">
@@ -102,11 +97,9 @@ const Hero = () => {
 								</div>
 							</div>
 						</div>
-						{/* <!-- End Item --> */}
 					</div>
 				</div>
 
-				{/* <!-- Arrows --> */}
 				<button
 					type="button"
 					className="hs-carousel-prev hs-carousel:disabled:opacity-50 disabled:pointer-events-none absolute inset-y-0 start-0 inline-flex justify-center items-center w-12 h-full text-black hover:bg-white/20 rounded-s-2xl focus:outline-none focus:bg-white/20">
@@ -144,10 +137,40 @@ const Hero = () => {
 						</svg>
 					</span>
 				</button>
-				{/* <!-- End Arrows --> */}
 			</div>
 		</div>
-		// <!-- End Slider -->
+	);
+}; */
+
+const HeroContent = () => {
+	return (
+		<div className="relative z-10 flex flex-col items-start justify-end md:justify-center h-full p-8 text-left text-white md:w-1/3 lg:w-1/4 font-sans">
+			<h1 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-2 md:mb-4">
+				Your Dream JDM Car Awaits
+			</h1>
+			<p className="text-lg md:text-xl lg:text-2xl mb-4">
+				Explore our exclusive collection of JDM imports.
+			</p>
+			<button className="w-full md:w-auto px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg transition duration-300">
+				View Inventory
+			</button>
+		</div>
+	);
+};
+
+const Hero = () => {
+	return (
+		<section className="relative w-full h-screen">
+			<div className="absolute inset-0">
+				<Image
+					src={heroImage}
+					alt="JDM Car"
+					className="w-full h-full object-cover"
+					priority
+				/>
+			</div>
+			<HeroContent />
+		</section>
 	);
 };
 
