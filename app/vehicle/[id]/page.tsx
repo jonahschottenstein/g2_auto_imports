961e655e074a54328dda76f83960719fa57703f7
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-08-24 15:25:05 -0600

    Use Node to pass images to CarPage

--- app/vehicle/[id]/page.tsx
+++ app/vehicle/[id]/page.tsx
@@ -3,8 +3,32 @@ import { inventory } from "@/inventory";
 import { Car } from "@/types";
 import CarPage from "@/components/CarPage";
 import { FormProvider } from "@/context/request-import-form-context";
+import { promises as fs } from "fs";
+import path from "path";
 
-const Page = ({ params }: { params: { id: string } }) => {
+const Page = async ({ params }: { params: { id: string } }) => {
+	const vehicleDetails: Car | undefined = inventory.find(
+		({ id }) => id === params.id
+	);
+	// * Not sure if you typed this correctly
+
+	const imageDirectory = path.join(
+		process.cwd(),
+		`/public/images/${vehicleDetails?.id}`
+	);
+	const imageFileNames = (await fs.readdir(imageDirectory)).map(
+		(fileName) => `/images/${vehicleDetails?.id}/${fileName}`
+	);
+
+	return (
+		<main className="flex-1 pt-[72px]">
+			<FormProvider>
+				<CarPage vehicleDetails={vehicleDetails} images={imageFileNames} />
+			</FormProvider>
+		</main>
+	);
+};
+/* const Page = ({ params }: { params: { id: string } }) => {
 	const vehicleDetails: Car | undefined = inventory.find(
 		({ id }) => id === params.id
 	);
@@ -17,6 +41,6 @@ const Page = ({ params }: { params: { id: string } }) => {
 			</FormProvider>
 		</main>
 	);
-};
+}; */
 
 export default Page;
