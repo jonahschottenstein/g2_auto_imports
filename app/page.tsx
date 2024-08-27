ead3ce9d39299e56572705a064e04b95e2b5ca4e
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-08-26 19:47:16 -0600

    Update todo list

--- app/page.tsx
+++ app/page.tsx
@@ -81,9 +81,9 @@ MakeModelSelector doesn't scroll to selected item when Request Import link is cl
 	- Need stateValue dependency to get MakeModelSelector to scroll to selected item on page visit
 	- But when it's a dependency, it also scrolls the item to the bottom every time an item is clicked
 	- Use IntersectionObserver
-Makes page Nav buttons are higher up than on Model page
-	- Because on mobile you can't see them without scrolling
-		- Need to figure out what to do for this
+// Makes page Nav buttons are higher up than on Model page
+// 	- Because on mobile you can't see them without scrolling
+// 		- Need to figure out what to do for this
 Don't like how What's Tokyo-Imports? => Your Gateway to Authentic JDM Imports looks on home page
 // - Now the years page and post-request page are too small
 ASK YOURSELF: WHAT ISN'T WORKING? FIX THAT FIRST
@@ -100,8 +100,8 @@ In order:
 */
 /* 
 	- Form has autofill on Chrome but not Safari. May need to add manually for Safari
-	- Nav Buttons for MakesForm and ModelsForm are in different places
-	- Years Form Nav Buttons overflow screen
+	// - Nav Buttons for MakesForm and ModelsForm are in different places
+	// - Years Form Nav Buttons overflow screen
 	- //! Inventory page shows Footer. Not sure if I want that
 	// - Need to add Request Import Button on vehicle page
 	// 	- Start form process, filling in info up to ContactForm
@@ -118,19 +118,19 @@ In order:
 	// - Right now, ProductionForm is wider than the rest
 	// 	- Back/Next buttons are further apart
 	- Don't know if I should add pt-[72px] to this <main> element like I did for the Form pages
-	- If you refresh Indeed form, it takes you back to the beginning
+	// - If you refresh Indeed form, it takes you back to the beginning
 	//- Should have first and last name displays on review page because that's how you asked for that information
 	- Would be nice to add animation when switching Form pages
 	// - Should last name be required?
-	- Need to erase sessionStorage data when leaving form
+	// - Need to erase sessionStorage data when leaving form
 	- Need to set browser back/forward buttons when filling out form so it doesn't go from Step_2 to Step_3 to Step_2, for example
 	- Need to figure out how to keep BackLink and NextLink at bottom of ProductionYears page on reload
 	// - If elements are selected using data from sessionStorage, scroll them into view
 	// 	- Think I need to use ref
 	// - Think I need to add form data to session storage in case page is reloaded
-	- Consider putting FormStepper back in layout.tsx. Think it'd be ok to have visible on PostRequest page
-		- And if it helps performance, it'd probably be worth it
-		- Don't know if I'll be able to do anything from layout.tsx about page reload though
+	// - Consider putting FormStepper back in layout.tsx. Think it'd be ok to have visible on PostRequest page
+		// - And if it helps performance, it'd probably be worth it
+		// - Don't know if I'll be able to do anything from layout.tsx about page reload though
 	- On Form Submission:
 		- on-screen confirmation that form was submitted
 		- automatic email sent to user
@@ -156,12 +156,12 @@ In order:
 	// - Add page for View Inventory link
 		// - Don't need to worry about now, but toprank imports has a filter for the inventory. That could be something to add at some point.
 			// - Will want to do it differently than they do, though, because there is an option for model that doesn't work until you select make. I think I would want the model filter to be invisible until make is selected
-	- Figure out Hero height styling
+	// - Figure out Hero height styling
 	- Make Form component (DRY)
-	- ProductionYearsSelector should maybe use grid auto columns
+	// - ProductionYearsSelector should maybe use grid auto columns
 	// - Don't know if you need "Request Import" <h1> on the Form pages. Takes up a lot of space
 	- form-nav-container with BackLink and NextLink should probably be its own component
-	- Fix Back/Next button placement in Form
+	// - Fix Back/Next button placement in Form
 	// - Think you should put Back/Next Links inside <Form>
 	- Figure out how to change error messages in ContactForm
 	- Figure out if you should disable submit button before inputs are completed
