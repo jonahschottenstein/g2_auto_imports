ab198b0c8a1f63ad084f1b4e4170a1e0386f2a32
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-08-28 21:10:15 -0600

    Add ContactPageContactForm component

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -9,7 +9,7 @@ import {
 import { zodResolver } from "@hookform/resolvers/zod";
 import axios from "axios";
 import Link from "next/link";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { FieldError, UseFormRegister, useForm } from "react-hook-form";
 import FormField from "./FormField";
 import {
@@ -20,8 +20,198 @@ import { BackLink, NextLink } from "./CustomLinks";
 import CustomButton from "./CustomButton";
 import { usePathname, useRouter } from "next/navigation";
 import FormContainer from "./FormContainer";
+import emailjs from "@emailjs/browser";
 
 // TODO: Think I need to make CustomButton accept children instead of title so I can pass svg to it
+
+export const ContactPageContactForm = () => {
+	const user = useFormContext();
+	const updateUserData = useFormUpdater();
+	const formRef = useRef<HTMLFormElement>(null);
+	const router = useRouter();
+
+	const values = user.contactInfo;
+	// TODO: Figure out if you're supposed to use user.contactInfo
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		setError,
+	} = useForm<ContactFormData>({
+		resolver: zodResolver(UserSchema),
+		values,
+	});
+
+	const sendEmail = (form: HTMLFormElement) => {
+		console.log("SEND EMAIL");
+		if (!formRef.current) return;
+		console.log("SEND EMAIL 2");
+
+		emailjs
+			.sendForm("import_form_service", "import_form", form, {
+				publicKey: "mGa2zHLRjc8S7ytXl",
+			})
+			.then(
+				() => {
+					console.log("SUCCESS");
+				},
+				(error) => {
+					console.log("FAILED...", error.text);
+				}
+			);
+	};
+
+	const onSubmit = async (data: ContactFormData) => {
+		console.log("SUCCESS", data);
+
+		try {
+			const response = await axios.post("/api/form", data); // Make a POST request
+			const { errors = {} } = response.data; // Destructure the 'errors' property from the response data
+			console.log("ERRORS", errors);
+
+			// Define a mapping between server-side field names and their corresponding client-side names
+			const fieldErrorMapping: Record<string, ValidFieldNames> = {
+				firstName: "firstName",
+				lastName: "lastName",
+				email: "email",
+				phone: "phone",
+				// state: "state",
+				zipCode: "zipCode",
+				comments: "comments",
+			};
+
+			// Find the first field with an error in the response data
+			const fieldWithError = Object.keys(fieldErrorMapping).find(
+				(field) => errors[field]
+			);
+
+			// If a field with an error is found, update the form error state using setError
+			if (fieldWithError) {
+				// Use the ValidFieldNames type to ensure the correct field names
+				setError(fieldErrorMapping[fieldWithError], {
+					type: "server",
+					message: errors[fieldWithError],
+				});
+			} else {
+				const data = {
+					contactInfo: {
+						firstName: (
+							document.getElementById("first-name") as HTMLInputElement
+						).value,
+						lastName: (document.getElementById("last-name") as HTMLInputElement)
+							.value,
+						email: (document.getElementById("email") as HTMLInputElement).value,
+						phone: (document.getElementById("phone") as HTMLInputElement).value,
+						zipCode: (document.getElementById("zip-code") as HTMLInputElement)
+							.value,
+						comments: (
+							document.getElementById("comments") as HTMLTextAreaElement
+						).value,
+					},
+				};
+
+				sessionStorage.setItem("contactInfo", JSON.stringify(data));
+
+				router.push("/contact/post-contact");
+
+				sendEmail(formRef.current as HTMLFormElement);
+			}
+		} catch (error) {
+			console.log("ERROR", error);
+			alert("Submitting form failed!");
+		}
+	};
+
+	const formFieldsData: FormField2[] = [
+		{
+			key: "first-name",
+			label: "First Name",
+			type: "text",
+			inputId: "first-name",
+			name: "firstName",
+			register: register,
+			error: errors.firstName,
+		},
+		{
+			key: "last-name",
+			label: "Last Name",
+			type: "text",
+			inputId: "last-name",
+			name: "lastName",
+			register: register,
+			error: errors.lastName,
+		},
+		{
+			key: "email",
+			label: "Email",
+			type: "email",
+			inputId: "email",
+			name: "email",
+			register: register,
+			error: errors.email,
+		},
+		{
+			key: "phone",
+			label: "Phone",
+			type: "tel",
+			inputId: "phone",
+			name: "phone",
+			register: register,
+			error: errors.phone,
+		},
+		{
+			key: "zip-code",
+			label: "Zip Code",
+			type: "text",
+			inputId: "zip-code",
+			name: "zipCode",
+			register: register,
+			error: errors.zipCode,
+		},
+		{
+			key: "comments",
+			label: "Comments",
+			type: "textarea",
+			inputId: "comments",
+			name: "comments",
+			register: register,
+			error: errors.comments,
+		},
+	];
+
+	console.log("ERRORS", errors);
+
+	return (
+		<form
+			ref={formRef}
+			onSubmit={handleSubmit(onSubmit)}
+			className="contact-form flex flex-col h-[calc(100%-64px)] font-sans">
+			<FormFieldsContainer>
+				{formFieldsData.map((formFieldData: FormField2) => {
+					return (
+						<FormField
+							key={formFieldData.inputId}
+							label={formFieldData.label}
+							type={formFieldData.type}
+							inputId={formFieldData.inputId}
+							name={formFieldData.name}
+							register={formFieldData.register}
+							error={formFieldData.error}
+						/>
+					);
+				})}
+			</FormFieldsContainer>
+			<div className="py-4">
+				<CustomButton
+					type="submit"
+					styles="w-full justify-center py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
+					Submit
+				</CustomButton>
+			</div>
+		</form>
+	);
+};
 
 const ContactForm = () => {
 	const user = useFormContext();
@@ -281,6 +471,7 @@ export const ContactForm2 = ({
 }: ContactForm2Props) => {
 	const user = useFormContext();
 	const updateUserData = useFormUpdater();
+	const formRef = useRef<HTMLFormElement>(null);
 	const router = useRouter();
 	const pathName = usePathname();
 
@@ -295,6 +486,41 @@ export const ContactForm2 = ({
 		resolver: zodResolver(UserSchema),
 		values,
 	});
+
+	const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		if (!formRef.current) return;
+
+		emailjs
+			.sendForm("import_form_service", "import_form", formRef.current, {
+				publicKey: "mGa2zHLRjc8S7ytXl",
+			})
+			.then(
+				() => {
+					console.log("SUCCESS");
+				},
+				(error) => {
+					console.log("FAILED...", error.text);
+				}
+			);
+	};
+
+	const onSubmitImportForm = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		const formStepper = document.querySelector("div.form-stepper");
+		const stepFiveItem = document.querySelector("li.form-stepper-item-5");
+
+		stepFiveItem?.classList.add("success");
+		formStepper?.classList.add("completed");
+
+		router.push("/request-import-form/post-request");
+
+		sendEmail(e);
+	};
+
+	const onSubmitContactForm = async (data: ContactFormData) => {};
 
 	const onSubmit = async (data: ContactFormData) => {
 		console.log("SUCCESS", data);
@@ -368,28 +594,30 @@ export const ContactForm2 = ({
 					router.push("/contact/post-contact");
 				}
 
+				// sendEmail(e);
+
 				// if (pathName !== "/contact/form") {
-				if (pathName === "/request-import-form/step_4") {
-					const data = {
-						contactInfo: {
-							firstName: (
-								document.getElementById("first-name") as HTMLInputElement
-							).value,
-							lastName: (
-								document.getElementById("last-name") as HTMLInputElement
-							).value,
-							email: (document.getElementById("email") as HTMLInputElement)
-								.value,
-							phone: (document.getElementById("phone") as HTMLInputElement)
-								.value,
-							zipCode: (document.getElementById("zip-code") as HTMLInputElement)
-								.value,
-							comments: (
-								document.getElementById("comments") as HTMLTextAreaElement
-							).value,
-						},
-					};
-					/* const storedUserData = sessionStorage.getItem("userData");
+				// if (pathName === "/request-import-form/step_4") {
+				// 	const data = {
+				// 		contactInfo: {
+				// 			firstName: (
+				// 				document.getElementById("first-name") as HTMLInputElement
+				// 			).value,
+				// 			lastName: (
+				// 				document.getElementById("last-name") as HTMLInputElement
+				// 			).value,
+				// 			email: (document.getElementById("email") as HTMLInputElement)
+				// 				.value,
+				// 			phone: (document.getElementById("phone") as HTMLInputElement)
+				// 				.value,
+				// 			zipCode: (document.getElementById("zip-code") as HTMLInputElement)
+				// 				.value,
+				// 			comments: (
+				// 				document.getElementById("comments") as HTMLTextAreaElement
+				// 			).value,
+				// 		},
+				// 	};
+				/* const storedUserData = sessionStorage.getItem("userData");
 					const userData = storedUserData && JSON.parse(storedUserData);
 					const newUserData = {
 						...userData,
@@ -401,14 +629,14 @@ export const ContactForm2 = ({
 						JSON.stringify({ ...newUserData })
 					); */
 
-					updateUserData(data);
+				// 	updateUserData(data);
 
-					router.push("/request-import-form/step_5");
-				} else {
-					console.log("Go to post-contact page");
-					console.log("Send emails");
-					router.push("/contact/post-contact");
-				}
+				// 	router.push("/request-import-form/step_5");
+				// } else {
+				// 	console.log("Go to post-contact page");
+				// 	console.log("Send emails");
+				// 	router.push("/contact/post-contact");
+				// }
 			}
 		} catch (error) {
 			console.log("ERROR", error);
