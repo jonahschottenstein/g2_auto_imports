33b43f9dcc5d26ada14c10dd2c562bb87d40d648
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-08-28 21:14:14 -0600

    Add PostContact component to page

--- app/contact/post-contact/page.tsx
+++ app/contact/post-contact/page.tsx
@@ -1,18 +1,10 @@
+import PostContact from "@/components/PostContact";
 import React from "react";
 
 const page = () => {
 	return (
 		<div className="form-page min-h-[calc(100vh-72px)]">
-			<div className="form-container px-8 h-full flex flex-col">
-				<h1 className="text-center text-2xl my-4 font-display">
-					Your contact information has been submitted!
-				</h1>
-				<div>
-					<p className="font-sans">
-						You will receive an email confirmation at{" "}
-					</p>
-				</div>
-			</div>
+			<PostContact />
 		</div>
 	);
 };
