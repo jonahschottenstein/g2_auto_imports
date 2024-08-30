a88c2bae9db9fd4a7538cf5410c207fd1012400e
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-08-30 15:06:26 -0600

    Refactor to clone children with pathName prop
    Passing pathName to CarCard because the value of
    the sizes prop on the Image component will vary
    depending on the current page

--- components/CardGrid.tsx
+++ components/CardGrid.tsx
@@ -1,20 +1,34 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import React from "react";
+import React, { cloneElement, isValidElement } from "react";
 
 interface CardGridProps {
-	children: React.ReactNode;
+	children:
+		| React.ReactElement<CardGridChildProps>
+		| React.ReactElement<CardGridChildProps>[];
 }
 
-const CardGrid = ({ children }: CardGridProps) => {
+interface CardGridChildProps {
+	pathName: string | null;
+}
+
+const CardGrid: React.FC<CardGridProps> = ({ children }: CardGridProps) => {
 	const pathName = usePathname();
 	const gridStyles = pathName === "/" ? "" : "md:grid-cols-3 lg:grid-cols-4";
 
 	return (
 		<div
 			className={`card-grid grid grid-cols-1 sm:grid-cols-2 ${gridStyles} gap-4 md:gap-5 lg:gap-6`}>
-			{children}
+			{React.Children.map(children, (child) => {
+				if (pathName !== null && isValidElement<CardGridChildProps>(child)) {
+					if (typeof child.type === "function") {
+						return cloneElement(child, { pathName });
+					}
+				}
+
+				return child;
+			})}
 		</div>
 	);
 };
