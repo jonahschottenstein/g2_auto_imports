68ba70e106e7e6dbdc57dbd9cdcda7d9e6038ade
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-08-31 17:59:03 -0600

    Update h1 and styles to fix alignment

--- components/PostContact.tsx
+++ components/PostContact.tsx
@@ -12,11 +12,13 @@ const PostContact = () => {
 	console.log("CONTACT INFO", contactInfo);
 
 	return (
-		<div className="form-container px-8 h-full flex flex-col">
-			<h1 className="text-center text-2xl my-4 font-display">
-				Your import request has been submitted!
-			</h1>
-			<div>
+		<div className="form-container h-[calc(100%-20px)] flex flex-col max-w-screen-xl mx-auto">
+			<div className="mb-6 sm:mb-10 max-w-2xl text-center mx-auto">
+				<h1 className="text-center text-2xl font-display">
+					Your contact information has been received!
+				</h1>
+			</div>
+			<div className="flex flex-col pb-8 justify-between flex-1">
 				<p className="font-sans">
 					You will get an email confirmation at{" "}
 					<span className="font-bold">
@@ -25,16 +27,16 @@ const PostContact = () => {
 						{contactInfo.contactInfo.email}
 					</span>
 				</p>
+				<CustomButton
+					children="Return Home"
+					styles="mx-auto w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none sm:w-60 font-sans"
+					ariaLabel="Return to home page"
+					handleClick={() => {
+						router.push("/");
+						sessionStorage.clear();
+					}}
+				/>
 			</div>
-			<CustomButton
-				children="Return Home"
-				styles="mt-[29rem] mx-auto w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none sm:w-60 font-sans"
-				ariaLabel="Return to home page"
-				handleClick={() => {
-					router.push("/");
-					sessionStorage.clear();
-				}}
-			/>
 		</div>
 	);
 };
