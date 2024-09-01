8f190e5f3461534a730af5d1665566d2e2fd7b7f
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-08-31 18:00:06 -0600

    Update styles to fix alignment issues

--- app/contact/post-contact/page.tsx
+++ app/contact/post-contact/page.tsx
@@ -1,9 +1,16 @@
 import PostContact from "@/components/PostContact";
 import React from "react";
 
+// const page = () => {
+// 	return (
+// 		<div className="form-page min-h-[calc(100vh-74px)]">
+// 			<PostContact />
+// 		</div>
+// 	);
+// };
 const page = () => {
 	return (
-		<div className="form-page min-h-[calc(100vh-72px)]">
+		<div className="post-contact-page w-full px-4 md:px-6 lg:px-8 py-12 h-full min-h-[calc(100vh-74px)]">
 			<PostContact />
 		</div>
 	);
