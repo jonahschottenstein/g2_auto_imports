8d440cf7aade1307ba78f25705f631f66718dbe3
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-09-01 16:28:34 -0600

    Pass userEmail searchParam to PostContact

--- app/contact/post-contact/page.tsx
+++ app/contact/post-contact/page.tsx
@@ -8,10 +8,14 @@ import React from "react";
 // 		</div>
 // 	);
 // };
-const page = () => {
+const page = ({
+	searchParams,
+}: {
+	searchParams: { [key: string]: string };
+}) => {
 	return (
 		<div className="post-contact-page w-full px-4 md:px-6 lg:px-8 py-12 h-full min-h-[calc(100vh-74px)]">
-			<PostContact />
+			<PostContact userEmail={searchParams.email} />
 		</div>
 	);
 };
