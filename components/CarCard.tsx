7805ebf41962369ec02d2637c314e1767a247dea
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-09-02 14:58:02 -0600

    Refactor CarCard to conditionally use h2/h3 element

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -2,66 +2,6 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-/* const CarCard = ({
-	imageSrc,
-	year,
-	make,
-	model,
-	price,
-	pageUrl,
-}: CarCardProps) => {
-	return (
-		<Link
-			className="flex flex-col group bg-white border shadow-sm rounded-xl overflow-hidden hover:shadow-lg transition "
-			href={pageUrl}>
-			<div className="relative pt-[50%] sm:pt-[60%] lg:pt-[80%] rounded-t-xl overflow-hidden">
-				<Image
-					className="size-full absolute top-0 start-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-t-xl"
-					src={imageSrc}
-					alt={`Image of ${year} ${make} ${model}`}
-					width={100}
-					height={100}
-				/>
-			</div>
-			<div className="p-4 md:p-5">
-				<h3 className="text-lg font-bold text-gray-800 ">
-					{`${year} ${make} ${model}`}
-				</h3>
-				<p className="mt-1 text-gray-500 ">{price}</p>
-			</div>
-		</Link>
-	);
-}; */
-
-/* const CarCard = ({
-	imageSrc,
-	year,
-	make,
-	model,
-	price,
-	pageUrl,
-}: CarCardProps) => {
-	return (
-		<Link
-			className="car-card flex flex-col shadow-md rounded-lg overflow-hidden transform transition-all duration-300 hover:scale-105 hover:shadow-lg"
-			href={pageUrl}>
-			<div className="relative pb-[66.6667%]">
-				<Image
-					className="absolute top-0 left-0 w-full h-full object-cover transition-transform duration-300"
-					src={imageSrc}
-					alt={`Image of ${year} ${make} ${model}`}
-					width={100}
-					height={100}
-				/>
-			</div>
-			<div className="p-4">
-				<h3 className="car-name text-xl text-gray-800 font-bold font-display uppercase">{`${year} ${make} ${model}`}</h3>
-				<p className="text-lg font-sans text-gray-600">{`${price}`}</p>
-			</div>
-		</Link>
-	);
-}; */
-
 interface CarCardProps {
 	href: string;
 	image: string;
@@ -107,13 +47,21 @@ const CarCard = ({
 			</div>
 			<div className="car-card-details px-4 flex flex-col flex-1 justify-between">
 				<div className="car-card-info">
-					<h3 className="car-name text-lg font-semibold text-black mt-2 font-sans">
-						{name}
-					</h3>
+					{pathName === "/" ? (
+						<h3 className="car-name text-lg font-semibold text-black mt-2 font-sans">
+							{name}
+						</h3>
+					) : (
+						<h2 className="car-name text-lg font-semibold text-black mt-2 font-sans">
+							{name}
+						</h2>
+					)}
 					{/* <p className="car-card-features font-sans">{features.join(" | ")}</p> */}
 					<ul className="car-card-features-list">
 						{features.map((feature) => (
-							<li className="inline text-base text-gray-800 font-sans after:content-['_|_'] after:text-gray-400 last:after:content-none">
+							<li
+								key={feature}
+								className="inline text-base text-gray-800 font-sans after:content-['_|_'] after:text-gray-400 last:after:content-none">
 								{feature}
 							</li>
 						))}
