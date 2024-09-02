9d6034bd60019f7f892b262aa00f7cb9fd15a4fd
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-09-01 20:37:25 -0600

    Refactor PostRequest to fix alignment issues

--- components/PostRequest.tsx
+++ components/PostRequest.tsx
@@ -18,25 +18,29 @@ const PostRequest = () => {
 	}, []);
 
 	return (
-		<div className="form-container px-8 h-full flex flex-col">
-			<h1 className="text-center text-2xl my-4 font-display">
-				Your import request has been submitted!
-			</h1>
-			<div>
-				<p className="font-sans">
-					You will get an email confirmation at{" "}
-					<span className="font-bold">{user.contactInfo?.email}</span>
-				</p>
+		<div className="form-container h-full flex flex-col max-w-screen-xl mx-auto">
+			<div className="mb-6 sm:mb-10 max-w-2xl text-center mx-auto">
+				<h1 className="text-center text-2xl font-display">
+					Your import request has been submitted!
+				</h1>
 			</div>
-			<CustomButton
-				children="Return Home"
-				styles="mt-[29rem] mx-auto w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none sm:w-60 font-sans"
-				ariaLabel="Return to home page"
-				handleClick={() => {
-					router.push("/");
-					sessionStorage.clear();
-				}}
-			/>
+			<div className="flex flex-col pb-8 justify-between flex-1">
+				<div>
+					<p className="font-sans">
+						You will get an email confirmation at{" "}
+						<span className="font-bold">{user.contactInfo?.email}</span>
+					</p>
+				</div>
+				<CustomButton
+					children="Return Home"
+					styles="mx-auto w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none sm:w-60 font-sans"
+					ariaLabel="Return to home page"
+					handleClick={() => {
+						router.push("/");
+						sessionStorage.clear();
+					}}
+				/>
+			</div>
 		</div>
 	);
 };
