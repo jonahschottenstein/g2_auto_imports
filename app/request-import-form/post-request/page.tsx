2c850280dcd832e8a284dc2629e1472696b195f2
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-09-01 20:38:02 -0600

    Update styles to fix alignment issues

--- app/request-import-form/post-request/page.tsx
+++ app/request-import-form/post-request/page.tsx
@@ -3,7 +3,8 @@ import React from "react";
 
 const page = () => {
 	return (
-		<div className="form-page min-h-[calc(100vh-72px)]">
+		// <div className="form-page min-h-[calc(100vh-72px)]">
+		<div className="form-page w-full px-4 md:px-6 lg:px-8 py-12 h-full min-h-[calc(100vh_-_74px)]">
 			<PostRequest />
 		</div>
 	);
