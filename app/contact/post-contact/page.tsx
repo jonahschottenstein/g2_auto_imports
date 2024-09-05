fb348683ffc8da705767ede254fee07d0334375a
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-09-05 12:44:32 -0600

    Use flex instead of (min-)height for page height
    Also change py-12 to pt-12 and pb-6.
    
    Need to figure out if it looks ok having uneven
    vertical padding.

--- app/contact/post-contact/page.tsx
+++ app/contact/post-contact/page.tsx
@@ -1,20 +1,13 @@
 import PostContact from "@/components/PostContact";
 import React from "react";
 
-// const page = () => {
-// 	return (
-// 		<div className="form-page min-h-[calc(100vh-74px)]">
-// 			<PostContact />
-// 		</div>
-// 	);
-// };
 const page = ({
 	searchParams,
 }: {
 	searchParams: { [key: string]: string };
 }) => {
 	return (
-		<div className="post-contact-page w-full px-4 md:px-6 lg:px-8 py-12 h-full min-h-[calc(100vh-74px)]">
+		<div className="post-contact-page w-full flex flex-1 px-4 md:px-6 lg:px-8 pt-12 pb-6">
 			<PostContact userEmail={searchParams.email} />
 		</div>
 	);
--- app/request-import-form/post-request/page.tsx
+++ app/request-import-form/post-request/page.tsx
@@ -3,8 +3,7 @@ import React from "react";
 
 const page = () => {
 	return (
-		// <div className="form-page min-h-[calc(100vh-72px)]">
-		<div className="form-page w-full px-4 md:px-6 lg:px-8 py-12 h-full min-h-[calc(100vh_-_74px)]">
+		<div className="form-page w-full flex flex-1 px-4 md:px-6 lg:px-8 pt-12 pb-6">
 			<PostRequest />
 		</div>
 	);
