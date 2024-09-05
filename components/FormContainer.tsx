809323b532faa8fc6a2fa1c62d5f35bce0aaaa9f
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-09-05 12:52:52 -0600

    Update styles to fill remaining height of parent

--- components/FormContainer.tsx
+++ components/FormContainer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const FormContainer = ({ h1, children }: FormContainerProps) => {
 	return (
-		<div className="form-container px-8 h-[calc(100%-92px)] flex flex-col min-w-96 max-w-lg w-full">
+		<div className="form-container flex flex-col flex-1 min-w-96 max-w-lg w-full">
 			<div className="mb-6 sm:mb-10 max-w-2xl text-center mx-auto">
 				<h1 className="font-medium font-display text-black text-2xl uppercase sm:text-4xl ">
 					{h1}
