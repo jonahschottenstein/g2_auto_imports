52cbc7e23813fba6b72da7a88c21437e253de95e
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-09-05 12:50:57 -0600

    Update styles so .form-page properly fills screen

--- app/request-import-form/step_2/page.tsx
+++ app/request-import-form/step_2/page.tsx
@@ -17,9 +17,7 @@ const page = async ({
 	// });
 
 	return (
-		<div
-			// className="form-page flex flex-col flex-1 items-center overflow-x-hidden overflow-y-auto"
-			className="form-page flex flex-col flex-1 items-center overflow-x-hidden overflow-y-clip min-h-[calc(100vh-72px)] px-4 md:px-6 lg:px-8">
+		<div className="form-page w-full flex flex-col flex-1 items-center overflow-x-hidden px-4 md:px-6 lg:px-8">
 			<FormStepper steps={["Make", "Model", "Year(s)", "Contact", "Review"]} />
 			<ModelsForm models={models} />
 		</div>
