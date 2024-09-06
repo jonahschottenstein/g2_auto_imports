ee0132ebda297835fc634bb9c274fbd70750c5a0
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-09-05 21:55:22 -0600

    Add faq Page content

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,7 +1,45 @@
+import { FAQContent } from "@/components/FAQSection";
 import React from "react";
 
 const Page = () => {
-	return <div>Page</div>;
+	const faqData = [
+		{
+			question: "Question 1",
+			answer:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate quaerat veniam, debitis eos molestiae provident praesentium perspiciatis adipisci eaque. Iure quam tenetur ratione ex molestias totam laudantium quae placeat accusamus.",
+		},
+		{
+			question: "Question 2",
+			answer:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate quaerat veniam, debitis eos molestiae provident praesentium perspiciatis adipisci eaque. Iure quam tenetur ratione ex molestias totam laudantium quae placeat accusamus.",
+		},
+		{
+			question: "Question 3",
+			answer:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate quaerat veniam, debitis eos molestiae provident praesentium perspiciatis adipisci eaque. Iure quam tenetur ratione ex molestias totam laudantium quae placeat accusamus.",
+		},
+		{
+			question: "Question 4",
+			answer:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate quaerat veniam, debitis eos molestiae provident praesentium perspiciatis adipisci eaque. Iure quam tenetur ratione ex molestias totam laudantium quae placeat accusamus.",
+		},
+		{ question: "Question 5", answer: "Answer 5" },
+	];
+
+	return (
+		<main className="flex-1 pt-[74px]">
+			<div className="faq-page w-full px-4 md:px-6 lg:px-8 py-12 h-full min-h-[calc(100vh_-_74px)]">
+				<div className="mb-6 sm:mb-10 max-w-2xl mx-auto text-center">
+					<h1 className="font-medium font-display text-black text-2xl uppercase sm:text-4xl">
+						Frequently Asked Questions
+					</h1>
+				</div>
+				<div className="faq-page-content max-w-screen-xl mx-auto">
+					<FAQContent faqData={faqData} faqCategory="Category" />
+				</div>
+			</div>
+		</main>
+	);
 };
 
 export default Page;
