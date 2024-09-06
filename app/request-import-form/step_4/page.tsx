b69a1f57cd37f7fc537bdbbf9cbf6da81800eb8b
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-09-06 17:54:31 -0600

    Update FormStepper steps and add ContactForm3

--- app/request-import-form/step_4/page.tsx
+++ app/request-import-form/step_4/page.tsx
@@ -1,4 +1,4 @@
-import ContactForm from "@/components/ContactForm";
+import ContactForm, { ContactForm3 } from "@/components/ContactForm";
 import FormStepper from "@/components/FormStepper";
 import React from "react";
 
@@ -7,8 +7,10 @@ const page = () => {
 		<div
 			// className="form-page flex flex-col flex-1 items-center overflow-x-hidden overflow-y-auto"
 			className="form-page flex flex-col flex-1 items-center overflow-x-hidden overflow-y-clip min-h-[calc(100vh-72px)] px-4 md:px-6 lg:px-8">
-			<FormStepper steps={["Make", "Model", "Year(s)", "Contact", "Review"]} />
-			<ContactForm />
+			{/* <FormStepper steps={["Make", "Model", "Year(s)", "Contact", "Review"]} /> */}
+			<FormStepper steps={["Make", "Model", "Years", "Contact"]} />
+			{/* <ContactForm /> */}
+			<ContactForm3 />
 		</div>
 	);
 };
