4ae5f8814eaedb107097ddd977cc2c5f900f3450
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-09-06 18:00:00 -0600

    Add FormField2 for ContactForm3

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FormFieldProps } from "@/types";
+import { FormFieldProps, FormFieldProps2 } from "@/types";
 import { FieldError } from "react-hook-form";
 
 const FormField = ({
@@ -73,3 +73,81 @@ const FormField = ({
 };
 
 export default FormField;
+
+export const FormField2 = ({
+	label,
+	type,
+	inputId,
+	name,
+	register,
+	error,
+	valueAsNumber,
+	rule,
+	areCommentsRequired,
+}: FormFieldProps2) => {
+	const inputMode =
+		inputId === "email"
+			? "email"
+			: inputId === "phone" || inputId === "zip-code"
+			? "numeric"
+			: "text";
+	// const afterLabelContent =
+	// 	type === "textarea"
+	// 		? "after:content-['(optional)']"
+	// 		: "after:content-['(required)']";
+	const afterLabelContent =
+		type === "textarea" && !areCommentsRequired
+			? "after:content-['(optional)']"
+			: "after:content-['(required)']";
+
+	const setInputStyles = (error: FieldError | undefined) =>
+		error
+			? "p-1 font-sans bg-transparent border-2 border-red-600 rounded-lg"
+			: "p-1 font-sans bg-transparent border-2 border-gray-200 rounded-lg";
+
+	return (
+		// <div className="form-field-container flex flex-col">
+		<div
+			className={
+				type === "hidden"
+					? "form-field-container hidden"
+					: "form-field-container flex flex-col"
+			}>
+			{/* <label htmlFor={inputId} className="form-field-label"> */}
+			{label && (
+				<label
+					htmlFor={inputId}
+					className={`form-field-label font-sans ${afterLabelContent} after:ml-1 after:text-xs`}>
+					{label}
+					{/* {type === "textarea" ? `${label} ` : `* ${label}`} */}
+				</label>
+			)}
+			{type === "textarea" ? (
+				<textarea
+					id={inputId}
+					className={setInputStyles(error)}
+					required={areCommentsRequired}
+					maxLength={500}
+					{...register(name, { maxLength: 500 })}
+					// TODO: Think I should set a max height/rows for textarea
+				/>
+			) : (
+				<input
+					type={type}
+					inputMode={inputMode}
+					id={inputId}
+					// className="p-1 font-sans bg-transparent border-2 border-gray-200 rounded-lg"
+					className={setInputStyles(error)}
+					required
+					{...register(name, { required: true, valueAsNumber })}
+				/>
+			)}
+			{/* {rule && <p className="rule text-xs">{rule}</p>} */}
+			{error && (
+				<span className="error-message font-sans text-sm text-red-600">
+					{error.message}
+				</span>
+			)}
+		</div>
+	);
+};
