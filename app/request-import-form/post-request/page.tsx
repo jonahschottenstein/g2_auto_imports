f6e6352d171892c172acb8f1d8e47611c27eacd2
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-09-08 19:03:36 -0600

    Pass searchParams.email to PostRequest

--- app/request-import-form/post-request/page.tsx
+++ app/request-import-form/post-request/page.tsx
@@ -1,10 +1,14 @@
 import PostRequest from "@/components/PostRequest";
 import React from "react";
 
-const page = () => {
+const page = ({
+	searchParams,
+}: {
+	searchParams: { [key: string]: string };
+}) => {
 	return (
 		<div className="form-page w-full flex flex-1 px-4 md:px-6 lg:px-8 pt-12 pb-6">
-			<PostRequest />
+			<PostRequest email={searchParams.email} />
 		</div>
 	);
 };
