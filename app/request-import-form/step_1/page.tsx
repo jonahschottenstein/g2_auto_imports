2deedd709cdf651fe66f3bfc5f5f88910f63605a
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-09-08 19:08:07 -0600

    Update FormStepper steps prop

--- app/request-import-form/step_1/page.tsx
+++ app/request-import-form/step_1/page.tsx
@@ -12,7 +12,8 @@ const page = async () => {
 
 	return (
 		<div className="form-page w-full flex flex-col flex-1 items-center overflow-x-hidden px-4 md:px-6 lg:px-8">
-			<FormStepper steps={["Make", "Model", "Year(s)", "Contact", "Review"]} />
+			{/* <FormStepper steps={["Make", "Model", "Year(s)", "Contact", "Review"]} /> */}
+			<FormStepper steps={["Make", "Model", "Years", "Contact"]} />
 			<MakesForm makes={makes} />
 		</div>
 	);
