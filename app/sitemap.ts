715230e3b0155ff1b942ae3c8c2f7847a2cffb26
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-10-07 16:47:02 -0600

    Use BASE_URL variable & comment out steps 2-4

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,32 +3,33 @@ import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 	const inventoryEntries: MetadataRoute.Sitemap = inventory.map(({ id }) => ({
-		url: `https://g2autoimports.com/vehicle/${id}`,
+		url: `${process.env.NEXT_PUBLIC_BASE_URL}/vehicle/${id}`,
 	}));
 
 	// ? Do I need to add post-request and post-contact pages?
+	// ? Should steps 2-4 be in sitemap?
 
 	return [
 		{
-			url: `https://g2autoimports.com/request-import-form/step_1`,
+			url: `${process.env.NEXT_PUBLIC_BASE_URL}/request-import-form/step_1`,
+		},
+		// {
+		// 	url: `https://g2autoimports.com/request-import-form/step_2`,
+		// },
+		// {
+		// 	url: `https://g2autoimports.com/request-import-form/step_3`,
+		// },
+		// {
+		// 	url: `https://g2autoimports.com/request-import-form/step_4`,
+		// },
+		{
+			url: `${process.env.NEXT_PUBLIC_BASE_URL}/inventory`,
 		},
 		{
-			url: `https://g2autoimports.com/request-import-form/step_2`,
+			url: `${process.env.NEXT_PUBLIC_BASE_URL}/faq`,
 		},
 		{
-			url: `https://g2autoimports.com/request-import-form/step_3`,
-		},
-		{
-			url: `https://g2autoimports.com/request-import-form/step_4`,
-		},
-		{
-			url: `https://g2autoimports.com/inventory`,
-		},
-		{
-			url: `https://g2autoimports.com/faq`,
-		},
-		{
-			url: `https://g2autoimports.com/contact/form`,
+			url: `${process.env.NEXT_PUBLIC_BASE_URL}/contact/form`,
 		},
 		...inventoryEntries,
 	];
