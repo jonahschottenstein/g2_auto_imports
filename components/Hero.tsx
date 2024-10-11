a0f53f29f85fac603d7cb1c7fdf320ea18ae0bdd
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-10-10 18:31:22 -0600

    Update Hero Image and styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,9 +4,11 @@ import Link from "next/link";
 // import heroMobile from "../public/images/hero/heroMobile.webp";
 // import heroDesktop from "../public/images/hero/heroDesktop.webp";
 // import starWagon from "../public/images/hero/hero_bg_just_green.jpg";
-// import desktopImage from "../public/images/hero/hero_desktop.jpeg";
-import heroD from "../public/images/hero/hero_d.webp";
+import desktopImage from "../public/images/hero/hero_desktop.jpeg";
+// import heroD from "../public/images/hero/hero_d.webp";
 import heroM from "../public/images/webp/hero/hero_star_wagon.webp";
+// import heroStylized from "../public/images/hero/hero_image_stylized_3 (1).webp";
+// import test3M from "../public/images/hero/hero_test_3_mobile.jpg";
 
 const MobileHeroImage = () => {
 	/* return (
@@ -43,12 +45,13 @@ const DesktopHeroImage = () => {
 	); */
 	return (
 		<Image
-			// src={desktopImage}
-			src={heroD}
+			src={desktopImage}
+			// src={heroD}
+			// src={heroStylized}
 			alt="Green 1999 Mitsubishi Delica Star Wagon and white 1999 Mitsubishi Delica Space Gear"
-			className="w-full h-full object-cover pointer-events-none select-none hidden sm:block"
+			className="w-full h-full object-cover object-[82%_50%] pointer-events-none select-none sm:block"
 			fill
-			sizes="(max-width: 640px): 0px, 100vw"
+			sizes="100vw"
 			priority
 		/>
 	);
@@ -78,18 +81,21 @@ const HeroContent = () => {
 	); */
 	return (
 		<div className="w-full h-full max-w-screen-xl mx-auto">
-			<div className="relative z-10 flex flex-col items-center justify-start h-full py-8 text-center text-white font-sans">
-				<div className="hero-content-wrapper flex flex-col justify-start items-center text-center py-4 gap-2 text-white sm:mt-20 xl:mt-16">
+			{/* <div className="relative z-10 flex flex-col items-center justify-start h-full py-8 text-center text-white font-sans"> */}
+			<div className="relative z-10 flex flex-col items-center justify-start h-full py-8 text-center text-white font-sans translate-y-20 transition-transform xl:translate-y-10 [@media(max-height:500px)]:translate-y-0 [@media(max-height:500px)]:pt-0">
+				{/* <div className="hero-content-wrapper flex flex-col justify-start items-center text-center py-4 gap-2 text-white sm:mt-20 xl:mt-16"> */}
+				<div className="hero-content-wrapper flex flex-col justify-start items-center text-center py-4 gap-2 text-white">
 					{/* <h1 className="text-3xl text-center md:text-4xl lg:text-5xl font-bold mb-1">
 						You Pick the Car - We'll Handle the Rest
 					</h1> */}
 					{/* <h1 className="text-[4.5vw] text-center md:text-4xl lg:text-5xl font-bold mb-1">
 						You Pick the Car - We'll Handle the Rest
 					</h1> */}
+					{/* <h1 className="text-[4.5vw] text-center md:text-4xl font-bold mb-1"> */}
 					<h1 className="text-[4.5vw] text-center md:text-4xl font-bold mb-1">
-						You Pick the Car - We&apos;ll Handle the Rest
+						You Pick the Car. We&apos;ll Handle the Rest.
 					</h1>
-					<p className="text-lg md:text-xl lg:text-2xl mb-4 max-w-[80%] font-semibold">
+					<p className="text-base md:text-xl lg:text-2xl mb-4 max-w-[80%] font-semibold">
 						We&apos;ll source, import, title, and deliver your vehicle straight
 						to your door.
 					</p>
@@ -111,7 +117,7 @@ const Hero = () => {
 		<section className="relative w-full h-[var(--section-height)] px-4 md:px-6 lg:px-8">
 			<div className="absolute inset-0">
 				<div className="hero-image-wrapper relative w-full h-full">
-					<MobileHeroImage />
+					{/* <MobileHeroImage /> */}
 					<DesktopHeroImage />
 				</div>
 			</div>
