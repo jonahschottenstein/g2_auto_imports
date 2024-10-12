5c3243f29a682cabf1e7de991c2ed90258db36f4
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-10-12 13:40:25 -0600

    Initial commit

---
+++ faqs/index.ts
@@ -0,0 +1,23 @@
+const faqs = [
+	{
+		question: "How does payment work?",
+		answer:
+			"After receiving and confirming a quote, the customer will be asked to pay 50% of the cost upfront and 50% upon receiving the vehicle.",
+	},
+	{
+		question: "Where do you ship to?",
+		answer: "G2 Auto Imports will ship to any state besides California.",
+	},
+	{
+		question: "How old do the vehicles need to be to legally import them?",
+		answer:
+			"Vehicles need to be at least 25 years old in order for us to legally import them.",
+	},
+	{
+		question: "How do you ship vehicles?",
+		answer:
+			"We use roll-on/roll-off (RORO) ships. These ships are designed to carry wheeled vehicles and are efficient, safe, and environmentally friendly.",
+	},
+];
+
+export default faqs;
